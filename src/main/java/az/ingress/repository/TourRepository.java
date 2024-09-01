54432585455ae47e5196f2f05e14593c906ee7fa
rashad652 <krashad775@gmail.com>
2024-09-01 18:57:18 +0400

    n+1 problem resolved

--- src/main/java/az/ingress/repository/TourRepository.java
+++ src/main/java/az/ingress/repository/TourRepository.java
@@ -3,13 +3,23 @@ package az.ingress.repository;
 import az.ingress.entity.Tour;
 import az.ingress.entity.Traveler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Set;
 
 
 @Repository
 public interface TourRepository extends JpaRepository<Tour, Long> {
+    @Query("select t from Tour t " +
+            "join fetch t.destinations d " +
+            "join fetch t.guides g " +
+            "join fetch g.passport p " +
+            "join fetch t.travelers tr"
+    )
+    List<Tour> findAll();
+
 
     Set<Tour> findByTravelers(Traveler traveler);
 }
